1f7dd251a971506183799a15d24d2c2dd9f40f3c
L <110619033+unavailableisunavailable@users.noreply.github.com>
2022-12-05 20:16:53 +0800

    Add files via upload

---
+++ DylanDeSouzaWk13ExB/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace DylanDeSouzaWk13ExB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClick = new System.Windows.Forms.Button();
+            this.lblAnswerAverageHeight = new System.Windows.Forms.Label();
+            this.lblAverageHeight = new System.Windows.Forms.Label();
+            this.lstHeightGroups = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnClick
+            // 
+            this.btnClick.Location = new System.Drawing.Point(320, 127);
+            this.btnClick.Name = "btnClick";
+            this.btnClick.Size = new System.Drawing.Size(75, 23);
+            this.btnClick.TabIndex = 13;
+            this.btnClick.Text = "Click";
+            this.btnClick.UseVisualStyleBackColor = true;
+            this.btnClick.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblAnswerAverageHeight
+            // 
+            this.lblAnswerAverageHeight.AutoSize = true;
+            this.lblAnswerAverageHeight.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblAnswerAverageHeight.Location = new System.Drawing.Point(233, 132);
+            this.lblAnswerAverageHeight.Name = "lblAnswerAverageHeight";
+            this.lblAnswerAverageHeight.Size = new System.Drawing.Size(2, 15);
+            this.lblAnswerAverageHeight.TabIndex = 12;
+            // 
+            // lblAverageHeight
+            // 
+            this.lblAverageHeight.AutoSize = true;
+            this.lblAverageHeight.Location = new System.Drawing.Point(146, 132);
+            this.lblAverageHeight.Name = "lblAverageHeight";
+            this.lblAverageHeight.Size = new System.Drawing.Size(81, 13);
+            this.lblAverageHeight.TabIndex = 11;
+            this.lblAverageHeight.Text = "Average Height";
+            // 
+            // lstHeightGroups
+            // 
+            this.lstHeightGroups.FormattingEnabled = true;
+            this.lstHeightGroups.Location = new System.Drawing.Point(127, 180);
+            this.lstHeightGroups.Name = "lstHeightGroups";
+            this.lstHeightGroups.Size = new System.Drawing.Size(543, 95);
+            this.lstHeightGroups.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnClick);
+            this.Controls.Add(this.lblAnswerAverageHeight);
+            this.Controls.Add(this.lblAverageHeight);
+            this.Controls.Add(this.lstHeightGroups);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClick;
+        private System.Windows.Forms.Label lblAnswerAverageHeight;
+        private System.Windows.Forms.Label lblAverageHeight;
+        private System.Windows.Forms.ListBox lstHeightGroups;
+    }
+}
---
+++ DylanDeSouzaWk13ExB/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace DylanDeSouzaWk13ExB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        const int MIN_HEIGHT = 100, MAX_HEIGHT = 275;
+        int[] heights = new int[MIN_HEIGHT];
+        int int_total_height = 0, int_average_height = 0;
+        int[] height_groups = {120};
+        Random random_generator = new Random();
+        int int_random_num = 0, int_height_group = 120;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int_random_num = random_generator.Next(MIN_HEIGHT, MAX_HEIGHT + 1);
+            heights.Append(int_random_num);
+            for (int indexer = 0; indexer < heights.Length;)
+                if (heights.Length == MIN_HEIGHT)
+                {
+                    int_total_height = heights.Sum();
+                    int_average_height = int_total_height / MIN_HEIGHT;
+                }
+            for (int indexer = 0; height_groups[indexer] != 179; indexer ++)
+                {
+                    height_groups[indexer] += 19;
+                }
+            for (int indexer = 0; indexer < height_groups.Length; indexer++)
+                {
+                    //need add to listbox
+                }
+            
+        }
+    }
+}
